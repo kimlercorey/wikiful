a5da0f997cd4b0f37e3b74dc64182da22e760e7f
kimler <kimler@gmail.com>
2014-07-15 20:40:32 -0400

    Added Category.all.sample as solution to demo problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,20 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 # Create 8 seed categories
-
 categories = Category.create([
-  { name: 'History'}, {name: 'Biology'}, {name: 'Literature'},
-  { name: 'Mathematics'}, { name: 'Music Theory'}, { name: 'Computer Science'},
-  { name: 'Sociology'}, {name: 'Chemistry'}
+{ name: 'History'}, {name: 'Biology'}, {name: 'Literature'},
+{ name: 'Mathematics'}, { name: 'Music Theory'}, { name: 'Computer Science'},
+{ name: 'Sociology'}, {name: 'Chemistry'}
 ])
 
 # create 50 articles, with random titles, 250 words of content, and
-# # randomly assign one of the categories above to each article
-
+# randomly assign one of the categories above to each article
 for i in 0..49
-  title = Faker::Lorem.sentence(rand(2..10)).chomp('.')
-  content = Faker::Lorem.paragraph(word_count=250)
+title = Faker::Lorem.sentence(rand(2..10)).chomp('.')
+content = Faker::Lorem.paragraph(word_count=250)
 
-  category = Category.first(offset: rand(Category.count))
-  a = Article.create(title: title, content: content, categories: [category,])
-
+# randomly assign one of the categories we just created
+# category = Category.first(offset: rand(Category.count))
+category = Category.all.sample
+a = Article.create(title: title, content: content, categories: [category,])
 end
-
